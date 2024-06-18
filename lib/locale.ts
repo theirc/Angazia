3b63e709a07c2bf447c513a5df131ee4499bd060
Pascalsala <146017634+Pascalsala@users.noreply.github.com>
2024-06-18 12:28:37 +0300

    Update locale.ts

--- lib/locale.ts
+++ lib/locale.ts
@@ -7,16 +7,20 @@ export { Locale };
 
 // Keep LOCALES and 'getLocaleFromCode' in sync with locales configured in /next.config.js.
 export const LOCALES: { [key: string]: Locale } = {
-  'en-us': { url: 'en-us', direction: 'ltr', name: 'English', directus: 'en-us' },
-  // sw: { url: 'sw', direction: 'ltr', name: 'Kiswahili', directus: 'sw-SW' },
-  // TODO add Kirundi
+  'en-us': {
+    url: 'en-us',
+    direction: 'ltr',
+    name: 'English',
+    directus: 'en-US',
+  },
+  sw: { url: 'sw', direction: 'ltr', name: 'Kiswahili', directus: 'sw-SW' },
 };
 
 export const LOCALE_CODES_TO_CANONICAL_LOCALE_CODES: { [key: string]: string } =
   {
     en: 'en-us',
     'en-us': 'en-us',
-    // sw: 'sw',
+    sw: 'sw',
   };
 
 // Returns the effective locale given locale code.
