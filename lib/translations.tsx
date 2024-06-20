0364f8e9940ae659500b06c784ddf805a2598b93
Pascalsala <146017634+Pascalsala@users.noreply.github.com>
2024-06-20 10:50:43 +0300

    Update translations.tsx

--- lib/translations.tsx
+++ lib/translations.tsx
@@ -42,10 +42,7 @@ export const COMMON_DYNAMIC_CONTENT_PLACEHOLDERS = [
 
 export const HOME_PAGE_DYNAMIC_CONTENT_PLACEHOLDERS = [
   // Header banner and social media strings.
-  /*
-   * TODO: create Dynamic content ID for mission statement.
-  '<website_name>_mission_statement',
-   */
+  'angazia_mission_statement',
   'default_banner_social_media_title',
   'default_banner_social_media_description',
   'default_banner_facebook_title',
